8082fcfd5079f98ee9d0e5b3ef7acb67a845e4ee
Nitha <nitha.p.joseph@gmail.com>
2025-12-03 21:04:19 +0400

    Remove secrets.py from repo and add to .gitignore

--- secrets.py
+++
@@ -1,6 +0,0 @@
-# --- Configuration (Environment variables for ECS) ---
-# DB_HOST = os.environ.get("DB_HOST", "news-sentiment-db-instance-2.crokgysyorow.ap-south-1.rds.amazonaws.com")
-# DB_NAME = os.environ.get("DB_NAME", "postgres")
-# DB_USER = os.environ.get("DB_USER", "postgres")
-# DB_PASSWORD = os.environ.get("DB_PASSWORD", "postgres_root")
-# DB_PORT = os.environ.get("DB_PORT", "5432")
